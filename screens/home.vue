13a2600f6bf7dd62ee2f39cade74ec6b4cec14bd
Dan Mills <hello@sandmill.org>
2019-06-24 00:36:05 -0700

    add more assets

--- screens/home.vue
+++ screens/home.vue
@@ -3,8 +3,7 @@
     <view class="top">
       <bar-code-scanner :type="type" :on-bar-code-scanned="onScanned" />
       <view>
-        <image class="logo"
-               :source="{uri: 'https://tinkerkitchen.org/images/Logo - orange - url.png'}" />
+        <image class="logo" :source="logoImg" />
       </view>
       <text class="welcome">Welcome! Hold your pass/ticket to the camera to scan</text>
       <view class="nopass-callout">
@@ -12,8 +11,7 @@
         <awesome-button type="primary" :on-press="goToForm">Tap to Check In</awesome-button>
       </view>
     </view>
-    <image class="qr-code"
-           :source="{uri: 'https://tinkerkitchen.org/images/Check in QR code.png'}"/>
+    <image class="qr-code" :source="qrCodeImg"/>
   </scroll-view>
 </template>
 
@@ -21,12 +19,15 @@
 import AwesomeButton from 'react-native-really-awesome-button/src/themes/blue';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import * as Permissions from 'expo-permissions';
+import { gql } from 'apollo-boost';
+
+import logoImg from "../assets/logo.png";
+import qrCodeImg from "../assets/qr-code.png";
 
 export default {
   props: {
-    navigation: {
-      type: Object
-    }
+    navigation: { type: Object },
+    screenProps: { type: Object },
   },
   components: {
     AwesomeButton,
@@ -36,7 +37,9 @@ export default {
     return {
       hasCameraPermission: null,
       type: BarCodeScanner.Constants.Type.front,
-      data: null,
+      qrData: null,
+      logoImg,
+      qrCodeImg
     };
   },
   async mounted() {
@@ -50,16 +53,50 @@ export default {
         backgroundColor: 'white',
         flex: 1,
       };
-      // justify-content: space-evenly;
-
     },
   },
   methods: {
-    onScanned({ type, data }) {
-      if (this.data) return;
+    async onScanned({ type, data }) {
+      if (this.qrData) return;
+      this.qrData = data;
 
-      this.data = data;
-      alert(`Scanned type: ${type}\nValue: ${data}`);
+      try {
+        const ret = await this.screenProps.client.mutate({
+          mutation: gql`
+            mutation checkInQrScan($qr_data: String!) {
+              check_in_qr_scan(qr_data: $qr_data) {
+                type
+                name
+                email
+                purchase_name
+                purchase_email
+                membership_status
+                consumable_status
+              }
+            }
+          `,
+          variables: { qr_data: data },
+        });
+        if (ret.data.check_in_qr_scan) {
+          const store = this.screenProps.store;
+          const qr = ret.data.check_in_qr_scan;
+          if (qr.type === 'user') {
+            // todo: check membership status
+            store.commit('setName', qr.name);
+            store.commit('setEmail', qr.email);
+            store.commit('setUserType', 'member');
+            store.commit('setChildName', '');
+            this.navigation.navigate('Form');
+          } else if (qr.type === 'daypass') {
+          } else if (qr.type === 'ticket') {
+          } else {
+            // unknown qr code type
+          }
+        }
+      } catch(e) {
+        // do nothing on invalid/unknown qr codes
+      }
+      this.qrData = null;
     },
 
     goToForm() {
